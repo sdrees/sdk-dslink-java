73ab1800053b3d0b16ec534fc4c2bbc644cf36d5
samrg472 <samrg472@gmail.com>
2016-02-10 17:40:22 -0800

    Bump to bug fix version 0.14.2-SNAPSHOT

--- ci/script/deploy.sh
+++ ci/script/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Deploy to maven
-if [ "$TRAVIS_BRANCH" != 'v0.14.x' ]; then
+if [ "$TRAVIS_BRANCH" != 'master' ]; then
     echo 'Ignoring artifact upload (not on master branch)'
 elif [ "$TRAVIS_PULL_REQUEST" == 'true' ]; then
     echo 'Ignoring artifact upload (pull request)'
